1251012d7147d81de391e68ff79b5b03e33a5fda
Jung ho Park <coool416@gmail.com>
2024-12-03 15:03:56 +0900

    Musical DB에서 데이터 가져오기 기능 구현

--- Back-end/muut/src/main/java/com/fp/muut/service/MainService.java
+++ Back-end/muut/src/main/java/com/fp/muut/service/MainService.java
@@ -2,10 +2,13 @@ package com.fp.muut.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fp.muut.dto.MusicalDTO;
+import com.fp.muut.entity.Musical;
 import com.fp.muut.repository.MainRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,11 +17,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MainService {
-	private final MainRepository mainRepository;
 
-	// 모든 뮤지컬 데이터 조회
-	public List<MusicalDTO> findmusicalDatas() {
-		return mainRepository.findAllMusicalData();
-	}
+    private final MainRepository mainRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(MainService.class);
+
+    // 모든 뮤지컬 데이터 조회
+    public List<Musical> findmusicalDatas() {
+        List<Musical> musicals = mainRepository.findAllMusicalData();
+        return musicals;
+    }
 }
