7b369d479d4b3e7af10d3bfd20713b6deb44b668
Jisoo-im <im.backh126@gmail.com>
2024-12-10 15:21:17 +0900

    로그인, 회원가입 css 수정

--- Front-end/main_page/src/login/Join.jsx
+++ Front-end/main_page/src/login/Join.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-// import './Join.css'
 import styles from "./Join.module.css";
 import axios from "axios";
+import Login from "./Login";
 
 
 export default function Join() {
+  const [currentView, setCurrentView] = useState("Join");
   const [customer_id, setCustomer_id] = useState(''); // 아이디 상태
   const [customer_pw, setCustomer_pw] = useState(''); // 비밀번호 상태
   
@@ -32,21 +33,27 @@ export default function Join() {
   };
 
     return (
+      <div className={styles.joinMain}>
+         {currentView === "Join" && (
       <>
         <header>
           <img src="./src/img/Muut_logo.png" witdh="300px" height="300px" ></img>
         </header>
-        <div>
+        <div className={styles.joinMain}>
           <h2>회원 정보를 입력해 주세요</h2>
           <p>이메일 주소를 아이디로 사용할게요.</p>
-          <form onSubmit={handleSubmit}>
-          <input type='text' value={customer_id} placeholder='아이디(이메일 형식으로 입력해주세요)'  onChange={handleIdChange}/><br/>
-          <input type='text' value={customer_pw} placeholder='비밀번호' onChange={handlePasswordChange}/><br/>
-          <button className={styles.join} type="submit">회원가입</button>
+          <form onSubmit={handleSubmit} className={styles.joinMain}>
+            <div><input type='text' className={styles.join_input} value={customer_id} placeholder='아이디(이메일 형식으로 입력해주세요)'  onChange={handleIdChange}/></div>
+            <div><input type='password' className={styles.join_input} value={customer_pw} placeholder='비밀번호' onChange={handlePasswordChange}/></div>
+            <button className={styles.join_btn} type="submit">회원가입</button>
           </form>
+          <hr/>
+          <button className={styles.login_btn} onClick={() => setCurrentView("Login")}>로그인</button>
         </div>
-  
-      </>
+        </>
+         )}
+         {currentView === "Login" && <Login />}
+      </div>
     )
   }
   
